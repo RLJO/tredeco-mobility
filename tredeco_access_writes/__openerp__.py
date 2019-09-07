d65c5a9551c469c2967f9aa57dbc630ee6828cb0
Mostafa Abd El Fattah <mostafa@white-code.co.uk>
2019-09-07 10:07:02 +0200

    add tredeco access writes

---
+++ tredeco_access_writes/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Tredco access writes",
+    'summary': """
+        """,
+    'description': """
+        
+    """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['sale', 'stock', 'purchase','repair'],
+
+    # always loaded
+    'data': [
+        
+        'views/purchase.xml',
+        'views/sale_order.xml',
+        'views/stock_picking.xml',
+        'views/repair.xml',
+    ],
+}
