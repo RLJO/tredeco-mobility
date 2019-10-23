59566df8a41f4ca9021d612fa52faa127a91b6a8
hardikcodetrade <hardik@codetrade.io>
2019-10-23 21:10:37 +0530

    [IMP]Hide the quality check and quality alert buttons if finished quantity and quantity to produce is not same

--- tredco_manufacturing_orders/models/manufacturing_order.py
+++ tredco_manufacturing_orders/models/manufacturing_order.py
@@ -3,9 +3,18 @@ from odoo import models, fields, api
 class MRpProduction(models.Model):
     _inherit = 'mrp.production'
 
+
+    show_quality_checks_and_alerts = fields.Boolean(compute='check_quantity')
+
     def do_un_produce(self):
         # Unlink the moves related to manufacture order
         moves = self.env['stock.move.line'].search([('reference', '=', self.name)]).unlink()
         self.state ='confirmed'
 
-
+    @api.multi
+    def check_quantity(self):
+        finished_product_quantity = sum(self.finished_move_line_ids.mapped('qty_done'))
+        if finished_product_quantity == self.product_qty:
+            self.show_quality_checks_and_alerts = True
+        else:
+            self.show_quality_checks_and_alerts = False
