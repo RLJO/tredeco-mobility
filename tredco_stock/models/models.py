e01cd5f14abdccef9d75b03f7599a83f0da3a8b4
hardikcodetrade <hardik@codetrade.io>
2019-11-20 12:46:38 +0530

    [IMP]Show warning when stock on hand is not enough in MO and internal transfer

--- tredco_stock/models/models.py
+++ tredco_stock/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError,ValidationError
 
 class StockPickingCT(models.Model):
     _inherit = "stock.picking"
@@ -11,4 +11,7 @@ class StockPickingCT(models.Model):
         self.ensure_one()
         if self.picking_type_code != 'incoming' and not all(self.move_ids_without_package.mapped('quantity_done')):
             raise UserError(_('You are not allowed to continue because there is a quantity = 0.'))
+        for move_id in self.move_ids_without_package:
+            if move_id.product_uom_qty > move_id.reserved_availability:
+                raise ValidationError(_('You are allowed to continue because there is not enough quantity in stock'))
         return super(StockPickingCT, self).button_validate()
