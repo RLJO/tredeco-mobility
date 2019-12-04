e29dbdae13dd6309be3cfdf73fc5d90384c0fc4b
hardikcodetrade <hardik@codetrade.io>
2019-12-04 17:40:01 +0530

    [FIX]tredco_stock : Fixed the issue of picking document not being validated

--- tredco_stock/models/models.py
+++ tredco_stock/models/models.py
@@ -9,9 +9,8 @@ class StockPickingCT(models.Model):
     @api.multi
     def button_validate(self):
         self.ensure_one()
-        if self.picking_type_code != 'incoming' and not all(self.move_ids_without_package.mapped('quantity_done')):
-            raise UserError(_('You are not allowed to continue because there is a quantity = 0.'))
-        for move_id in self.move_ids_without_package:
-            if move_id.product_uom_qty > move_id.reserved_availability:
-                raise ValidationError(_('You are allowed to continue because there is not enough quantity in stock'))
+        if self.picking_type_code in ['outgoing','internal']:
+            for move_id in self.move_ids_without_package:
+                if move_id.product_uom_qty > move_id.reserved_availability:
+                    raise ValidationError(_('You are allowed to continue because there is not enough quantity in stock'))
         return super(StockPickingCT, self).button_validate()
