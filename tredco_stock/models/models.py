1185a48a026cb6c46e9b01737be605918f801397
hardikcodetrade <hardik@codetrade.io>
2019-12-05 11:50:07 +0530

    [FIX] Warning on Stock Picking validate added when quantity done > quantity reserved.

--- tredco_stock/models/models.py
+++ tredco_stock/models/models.py
@@ -11,6 +11,6 @@ class StockPickingCT(models.Model):
         self.ensure_one()
         if self.picking_type_code in ['outgoing','internal']:
             for move_id in self.move_ids_without_package:
-                if move_id.product_uom_qty > move_id.reserved_availability:
+                if move_id.quantity_done > move_id.reserved_availability:
                     raise ValidationError(_('You are allowed to continue because there is not enough quantity in stock'))
         return super(StockPickingCT, self).button_validate()
