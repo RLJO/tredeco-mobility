d4d0f56dfb9748cbd34124ed388a0796107961fc
ibrahim.m <ibrahimmohamedd190@gmail.com>
2020-02-05 16:34:19 +0200

    update to show button validate inventory

--- tredeco_modification/__manifest__.py
+++ tredeco_modification/__manifest__.py
@@ -5,7 +5,8 @@
     'category': '',
     'description': """
    tredeco_modification used to show or hid  smart buttons cost analysis from product view and structure and cost from bill of materials and cost analysis smart buttons from manufacturing order 
-   and create security groups in user settings to control on display buttons or hid it
+   and create security groups in user settings to control on display buttons or hid it 
+   you will find this groups under Category called >>Show Cost analysis and cost structure Smart Buttons
 
     """,
     'depends' : ['base','mrp_account','mrp'],
